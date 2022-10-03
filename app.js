6ba4e5dbbbad76a8763626f84207cd6839bf1697
NearlyHeart <61740386+NearlyHeart@users.noreply.github.com>
2022-10-03 20:56:01 +0700

    routing: add navigate & create router

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan')
 const path = require('path');
+const productRouter = express.Router();
 
 const app = express();
 const port = process.env.PORT;
@@ -13,9 +14,18 @@ app.use(express.static(path.join(__dirname,"/public/")));
 app.set("views","./src/views");
 app.set("view engine", "ejs");
 
+productRouter.route("/").get((req,res) => {
+    res.send("Hello Products");
+});
+productRouter.route("/1").get((req,res) => {
+    res.send("Hello Products1");
+});
+
+app.use('/products', productRouter)
+
 app.get("/",(req, res) => {
 
-    res.render('index',{username: 'Erk', customers: ["phong","panot","phumphua"]});
+    res.render('index');
 
 })
 

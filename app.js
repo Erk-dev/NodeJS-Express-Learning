163fcbd5834138b96f66f3d85b61551fe33ee27d
NearlyHeart <61740386+NearlyHeart@users.noreply.github.com>
2022-10-12 11:58:36 +0700

    rendering: products adv data
    Lecture20: การส่งข้อมูลภายในเว็บแอปพลิเคชันสูง

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan')
 const path = require('path');
+const products = require('./data/products.json')
 const productRouter = express.Router();
 
 const app = express();
@@ -15,14 +16,9 @@ app.set("views","./src/views");
 app.set("view engine", "ejs");
 
 productRouter.route("/").get((req,res) => {
-    res.render("products", {
-        products: [
-            {productTitle: 'ซีพียู', productDescription: 'ซีพียูยี่ห้อ 1 ดีมาก', productPrice: 10000},
-            {productTitle: 'ซีพียู 2', productDescription: 'ซีพียูยี่ห้อ 2 ดีมาก', productPrice: 11000},
-            {productTitle: 'ซีพียู 3', productDescription: 'ซีพียูยี่ห้อ 3 ดีมาก', productPrice: 12000},
-            {productTitle: 'ซีพียู 4', productDescription: 'ซีพียูยี่ห้อ 4 ดีมาก', productPrice: 13000},
-        ],
-    });
+    res.render("products", 
+        products,
+    );
 });
 productRouter.route("/1").get((req,res) => {
     res.send("Hello Products1");
